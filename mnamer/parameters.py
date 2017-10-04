2f4069f9359dd42021440b0d76a12d870658d5e0
Jessy Williams <jessy@jessywilliams.com>
2017-10-04 19:51:47 -0400

    Uses _bool_parse in lieu of store store_true for boolean flags

--- mnamer/parameters.py
+++ mnamer/parameters.py
@@ -22,85 +22,94 @@ class Parameters:
             # usage=USAGE
         )
 
-        self._parser.add_argument(
-            '-b', '--batch', action='store_true', default=None,
+        parser.add_argument(
+            '-b', '--batch', nargs='?', type=self._bool_parse, const=True,
             help='batch mode; disables interactive prompts & persists on error'
         )
 
-        self._parser.add_argument(
-            '-d', '--dots', action='store_true', default=None,
+        parser.add_argument(
+            '-d', '--dots', nargs='?', type=self._bool_parse, const=True,
             help='format using dots in place of whitespace when renaming'
         )
 
-        self._parser.add_argument(
-            '-l', '--lower', action='store_true', default=None,
+        parser.add_argument(
+            '-l', '--lower', nargs='?', type=self._bool_parse, const=True,
             help='format using lowercase when renaming'
         )
 
-        self._parser.add_argument(
-            '-r', '--recurse', action='store_true', default=None,
+        parser.add_argument(
+            '-r', '--recurse', nargs='?', type=self._bool_parse, const=True,
             help='recursive file crawling and following symlinks'
         )
 
-        self._parser.add_argument(
-            '-v', '--verbose', action='store_true', default=None,
+        parser.add_argument(
+            '-v', '--verbose', nargs='?', type=self._bool_parse, const=True,
             help='increases output verbosity'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--extmask', nargs='+', metavar='E', default=None,
             help='define the extension mask used by the the file parser'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--max_hits', nargs='?', metavar='M', type=int, default=None,
             help='limits the maximum number of hits for each query'
         )
 
         # if find_spec('PyQt5'):
-        #     self._parser.add_argument(
-        #         '--ui', action='store_true', default=None,
+        #     parser.add_argument(
+        #         '--ui',  nargs='?', type=self._str2bool, const=True,
         #         help='set user interface mode'
         #     )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--movie_api', nargs='?', metavar='MA', choices=['imdb', 'tmdb'],
             default=None, help='set movie api provider'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--movie_destination', nargs='?', metavar='D', default=None,
             help='set movie relocation destination'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--movie_template', nargs='?', metavar='T', default=None,
             help='set movie renaming template'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--tv_api', nargs='?', metavar='TA', choices=['tvdb'],
             default=None, help='set television api provider'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--tv_destination', nargs='?', metavar='D', default=None,
             help='set television relocation destination'
         )
 
-        self._parser.add_argument(
+        parser.add_argument(
             '--tv_template', nargs='?', metavar='T', default=None,
             help='set television renaming template'
         )
 
-        self._parser.add_argument(
-            'targets', nargs='+', default=[],
+        parser.add_argument(
+            'targets', nargs='*', default=[],
             help='media files and/or directories'
         )
 
         args = vars(self._parser.parse_args())
         self._targets = args.pop('targets')
         self._arguments = {k: v for k, v in args.items() if v}
+
+    @staticmethod
+    def _bool_parse(s: O[str]):
+        if s.lower() in ('yes', 'true', 't', 'y', '1'):
+            return True
+        elif s.lower() in ('no', 'false', 'f', 'n', '0'):
+            return False
+        else:
+            raise argparse.ArgumentTypeError(f'Expected bool, got {type(s)}.')
 
     @property
     def targets(self) -> L[str]:
