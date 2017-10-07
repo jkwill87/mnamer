911eeffb599347df156bb888b1a2bd34e8f512a1
Jessy Williams <jessy@jessywilliams.com>
2017-10-07 18:34:25 -0400

    Adds 'mnamer' as console script
    ... so it can be run as an executable once installed

--- setup.py
+++ setup.py
@@ -8,17 +8,20 @@ with open('requirements.txt', 'r') as fp:
 with open('readme.rst', 'r') as fp:
     LONG_DESCRIPTION = fp.read()
 
-ABOUT = {
-    'author': 'Jessy Williams',
-    'author_email': 'jessy@jessywilliams.com',
-    'description': 'A media file organiser',
-    'license': 'MIT',
-    'long_description': LONG_DESCRIPTION,
-    'name': 'mnamer',
-    'packages': ['mnamer'],
-    'install_requires': REQUIREMENTS,
-    'url': 'https://github.com/jkwill87/mnamer',
-    'version': '0.1'
-}
-
-setup(**ABOUT)
+setup(
+    author='Jessy Williams',
+    author_email='jessy@jessywilliams.com',
+    description='A media file organiser',
+    entry_points={
+        'console_scripts': [
+            'mnamer=mnamer.__main__:main'
+        ]
+    },
+    license='MIT',
+    long_description=LONG_DESCRIPTION,
+    name='mnamer',
+    packages=['mnamer'],
+    install_requires=REQUIREMENTS,
+    url='https://github.com/jkwill87/mnamer',
+    version='0.2'
+)
