cdb4596285be4f0f1041ffb22371c33917e0a346
Jessy Williams <jessy@jessywilliams.com>
2017-10-09 19:45:00 -0400

    Reworks parameter arguments and adds override optionals

--- mnamer/parameters.py
+++ mnamer/parameters.py
@@ -2,26 +2,17 @@ import argparse
 
 from mnamer import *
 
-EPILOG = 'visit https://github.com/jkwill87/mnamer for more information'
-
-USAGE = """mnamer target [targets ...]
-  [-b, --batch] [-d, --dots] [-l, --lower] [-r, --recurse] [-v --verbose]
-  [-h, --help] [--extmask E] [--maxhits M] [--testrun] [--mapi MA] [--mdest MD]
-  [--mtemp MT] [--tapi TA] [--tdest TD] [--ttemp TT] [--sconf C] [--lconf]
-"""
-
 
 class Parameters:
     def __init__(self):
         self._parser = argparse.ArgumentParser(
             prog='mnamer',
-            epilog=EPILOG,
-            usage=USAGE
+            epilog='visit https://github.com/jkwill87/mnamer for more info',
         )
 
         self._parser.add_argument(
             '-b', '--batch', action='store_true',
-            help='batch mode; disables interactive prompts & persists on error'
+            help='batch mode; disables interactive prompts'
         )
 
         self._parser.add_argument(
@@ -45,55 +36,54 @@ class Parameters:
         )
 
         self._parser.add_argument(
-            '-t', '--testrun', dest='test_run', action='store_true',
+            '--testrun', dest='test_run', action='store_true',
             help='set movie api provider'
         )
 
         self._parser.add_argument(
-            '--extmask',
-            dest='ext_mask', nargs='+', metavar='E', default=None,
+            '--extmask', metavar='E', dest='ext_mask', nargs='+', default=None,
             help='define the extension mask used by the the file parser'
         )
 
         self._parser.add_argument(
-            '--maxhits',
-            dest='max_hits', nargs='?', metavar='M', type=int, default=None,
+            '--maxhits', metavar='#',
+            dest='max_hits', nargs=1, type=int, default=None,
             help='limits the maximum number of hits for each query'
         )
 
         self._parser.add_argument(
             '--mapi',
-            dest='movie_api', nargs='?', metavar='MA', choices=['imdb', 'tmdb'],
+            dest='movie_api', nargs=1, choices=['imdb', 'tmdb'],
             default=None, help='set movie api provider'
         )
 
         self._parser.add_argument(
-            '--mdest',
-            dest='movie_destination', nargs='?', metavar='D', default=None,
+            '--mdest', metavar='DEST',
+            dest='movie_destination', nargs=1, default=None,
             help='set movie relocation destination'
         )
 
         self._parser.add_argument(
-            '--mtemp',
-            dest='movie_template', nargs='?', metavar='T', default=None,
+            '--mtemp', metavar='TEMPLATE',
+            dest='movie_template', nargs=1, default=None,
             help='set movie renaming template'
         )
 
         self._parser.add_argument(
             '--tapi',
-            dest='television_api', nargs='?', metavar='TA', choices=['tvdb'],
+            dest='television_api', nargs=1, choices=['tvdb'],
             default=None, help='set television api provider'
         )
 
         self._parser.add_argument(
-            '--tdest',
-            dest='television_destination', nargs='?', metavar='D', default=None,
+            '--tdest', metavar='DEST',
+            dest='television_destination', nargs=1, default=None,
             help='set television relocation destination'
         )
 
         self._parser.add_argument(
-            '--ttemp',
-            dest='television_template', nargs='?', metavar='T', default=None,
+            '--ttemp', metavar='TEMPLATE',
+            dest='television_template', nargs=1, default=None,
             help='set television renaming template'
         )
 
@@ -103,18 +93,32 @@ class Parameters:
         )
 
         self._parser.add_argument(
-            '--sconf',
-            dest='save_config', nargs='?', metavar='C', const=config_file,
-            help=f'save configuration to file; defaults to {config_file}'
+            '--saveconfig', metavar='CONF',
+            dest='save_config', nargs='?', const=config_file,
+            help=f'save configuration to file'
         )
 
         self._parser.add_argument(
-            '--lconf',
-            dest='load_config', nargs='?', metavar='C', default=None,
+            '--loadconfig', metavar='CONF',
+            dest='load_config', nargs=1, default=None,
             help='load configuration from file'
         )
 
+        self._parser.add_argument(
+            '--id', nargs=1,
+            help='explicitly specify movie or series id'
+        )
+
+        self._parser.add_argument(
+            '--media', nargs=1, choices=['movie', 'television'],
+            help='override media detection; either movie or television'
+        )
+
         args = vars(self._parser.parse_args())
+        self._overrides = {
+            'id': args.pop('id', None),
+            'media': args.pop('media', None)
+        }
         self._save_config = args.pop('save_config', None)
         self._load_config = args.pop('load_config', None)
         self._targets = args.pop('targets')
@@ -136,6 +140,10 @@ class Parameters:
     def load_config(self) -> O[str]:
         if self._load_config:
             return self._load_config
+
+    @property
+    def overrides(self) -> D[str, str]:
+        return self._overrides
 
     def print_help(self):
         self._parser.print_help()
