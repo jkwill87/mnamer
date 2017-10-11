451b843aedd3a7ea8ddb7cc605ca6ed09934ea10
Jessy Williams <jessy@jessywilliams.com>
2017-10-11 19:47:56 -0400

    Adds sets to package-level typing import

--- mnamer/__init__.py
+++ mnamer/__init__.py
@@ -10,10 +10,13 @@ to fill in the blanks, and then renames and moves them.
 See https://github.com/jkwill87/mnamer for more information.
 """
 
-from typing import Any as A, Dict as D, List as L, Optional as O, Union as U
+from typing import (
+    Any as A,
+    Dict as D,
+    List as L,
+    Optional as O,
+    Set as S,
+    Union as U,
+)
 
-from appdirs import user_config_dir as _user_config_dir
-
-__all__ = ['config_file', 'A', 'D', 'L', 'O', 'U']
-
-config_file = f'{_user_config_dir()}/mnamer.json'
+__all__ = ['A', 'D', 'L', 'O', 'S', 'U']
