1b85f87ad9b3d28ee3018c0288301b01a7a9ad84
Jessy Williams <jessy@jessywilliams.com>
2017-10-13 21:03:42 -0400

    Requires python3 to run as module

--- mnamer/__init__.py
+++ mnamer/__init__.py
@@ -9,17 +9,19 @@ to fill in the blanks, and then renames and moves them.
 
 See https://github.com/jkwill87/mnamer for more information.
 """
+try:
+    from typing import (
+        Any as A,
+        Dict as D,
+        List as L,
+        Optional as O,
+        Set as S,
+        Union as U,
+    )
+except ImportError:
+    exit('mnamer: error: requires python 3.6+')
 
-from typing import (
-    Any as A,
-    Dict as D,
-    List as L,
-    Optional as O,
-    Set as S,
-    Union as U,
-)
-from appdirs import user_config_dir as _user_config_dir
+from appdirs import user_config_dir
 
-config_path = f'{_user_config_dir()}mnamer.json'
-
+config_path = user_config_dir() + 'mnamer.json'
 __all__ = ['A', 'D', 'L', 'O', 'S', 'U', 'config_path']
