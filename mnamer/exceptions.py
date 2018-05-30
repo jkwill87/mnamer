f1abc7a71c9008c6951ce20d122cce94da557d5f
Jessy Williams <jessy@jessywilliams.com>
2018-05-29 22:06:55 -0400

    Adds mnamer exception classes

---
+++ mnamer/exceptions.py
@@ -0,0 +1,7 @@
+class MnamerException(Exception):
+    """ Base exception for the mnamer package
+    """
+
+class MnamerConfigException(MnamerException):
+    """ Raised when an error has occurred either loading or saving a config file
+    """
