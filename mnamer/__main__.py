16ee789f04206cec6eea57cd0e1dc7f95a2c8817
Jessy Williams <jessy@jessywilliams.com>
2018-06-15 18:42:16 -0400

    Removes banner from __main__

--- mnamer/__main__.py
+++ mnamer/__main__.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-"""
-  _  _  _    _  _    __,   _  _  _    _   ,_
- / |/ |/ |  / |/ |  /  |  / |/ |/ |  |/  /  |
-   |  |  |_/  |  |_/\_/|_/  |  |  |_/|__/   |_/
-
-mnamer (Media reNAMER) is an intelligent and highly configurable media
-organization utility. It parses media filenames for metadata, searches the web
-to fill in the blanks, and then renames and moves them.
-
-See https://github.com/jkwill87/mnamer for more information.
-"""
-
 from __future__ import print_function
 
 from argparse import ArgumentParser
