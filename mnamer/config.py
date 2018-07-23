5301c9bae3532e7e1d8e6584b9ba1c38ff370471
Jessy Williams <jessy@jessywilliams.com>
2018-07-23 07:45:22 -0400

    Implements Configuration using attrs

--- mnamer/config.py
+++ mnamer/config.py
@@ -1,49 +1,96 @@
 from copy import deepcopy
+from json import dumps
 
-from attr import attrib, attrs, validate, validators
+from attr import attrib, attrs, fields_dict, validate, validators
 
-from mnamer.args import Arguments
+from mnamer.constants import DIRECTIVE_KEYS, PREFERENCE_KEYS
 from mnamer.exceptions import MnamerConfigException
 from mnamer.utils import config_find, config_load, merge_dicts
 
 
 @attrs
-class Config:
-    batch = attrib()
-    blacklist = attrib()
-    extmask = attrib()
-    hits = attrib()
-    recurse = attrib()
-    scene = attrib()
-    verbose = attrib()
-    movie_api = attrib()
-    movie_destination = attrib()
-    movie_template = attrib()
-    television_api = attrib()
-    televsion_destination = attrib()
-    television_template = attrib()
+class Configuration:
+    ## General Prefereneces
+    batch = attrib(False)
+    blacklist = attrib((".*sample.*", "^RARBG.*"))
+    extmask = attrib(("avi", "m4v", "mp4", "mkv", "ts", "wmv"))
+    hits = attrib(15)
+    recurse = attrib(False)
+    scene = attrib(False)
+    verbose = attrib(False)
 
-    def __init__(self, load_file=True):
-        self._data = deepcopy(Arguments.config)
-        self._load_error = None
-        self._loaded_file = None
+    ## Movie related
+    movie_api = attrib("tmdb")
+    movie_destination = attrib("")
+    movie_template = attrib("<$title >" "<($year)>" "<$extension>")
+
+    ## Television related
+    television_api = attrib("tvdb")
+    televsion_destination = attrib("")
+    television_template = (
+        attrib(
+            (
+                "<$series - >"
+                "< - S$season>"
+                "<E$episode - >"
+                "< - $title>"
+                "<$extension>"
+            )
+        ),
+    )
+
+    ## API Keys -- consider using your own or IMDb if limits are hit
+    api_key_tmdb = attrib("db972a607f2760bb19ff8bb34074b4c7")
+    api_key_tvdb = attrib("E69C7A2CEF2F3152")
+
+    # Directives
+    id = attrib(None)
+    media = attrib(None)
+    test = attrib(None)
+    version = attrib(None)
 
     def load_file(self):
-        try:
-            json_file = config_find()
-            json_data = config_load(json_file)
-            self._data = merge_dicts(self._data, json_data)
-            self._loaded_file = json_file
-        except MnamerConfigException as e:
-            self._load_error = e
+        json_file = config_find()
+        json_data = config_load(json_file)
+        for key in PREFERENCE_KEYS:
+            value = json_data.get(key)
+            if value is not None:
+                setattr(self, key, value)
+        return json_file
 
     @property
-    def loaded_file(self):
-        return self._loaded_file
+    def preference_dict(self):
+        return {
+            key: value
+            for key, value in fields_dict(self).items()
+            if key in PREFERENCE_KEYS
+        }
 
     @property
-    def load_error(self):
-        return self._load_error
+    def preference_json(self):
+        return dumps(
+            self.preference_dict, sort_keys=True, skipkeys=True, allow_nan=False
+        )
+
+    @property
+    def directive_dict(self):
+        return {
+            key: value
+            for key, value in fields_dict(self).items()
+            if key in DIRECTIVE_KEYS
+        }
+
+    @property
+    def directive_json(self):
+        return dumps(
+            self.directive_dict, sort_keys=True, skipkeys=True, allow_nan=False
+        )
+
+    @property
+    def config_dict(self):
+        return fields_dict(
+            merge_dicts(self.preference_dict, self.directive_dict)
+        )
 
     @property
     def validated(self):
