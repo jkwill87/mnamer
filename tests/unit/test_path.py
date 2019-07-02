2b6d91ce0db8bd79526381ad10fdaceb8fd28bfb
Jessy Williams <jwilliams@freshbooks.com>
2019-07-02 18:52:45 -0400

    Adds TestPath

---
+++ tests/unit/test_path.py
@@ -0,0 +1,44 @@
+from os import getcwd
+from os.path import join, abspath
+
+import pytest
+
+from mnamer.path import Path
+
+
+@pytest.mark.usefixtures("setup_test_path")
+class TestPath:
+    """ Unit tests for mnamer/path.py:Path
+    """
+
+    @pytest.fixture(autouse=True)
+    def _set_cwd(self):
+        self.cwd = getcwd()
+
+    def test_init__directory__relative(self):
+        path = "./avengers.mkv"
+        assert Path(path).directory == self.cwd
+
+    def test_init__directory__absolute(self):
+        path = join(self.cwd, "avengers.mkv")
+        assert Path(path).directory == self.cwd
+
+    def test_init__filename(self):
+        path = join(self.cwd, "avengers.mkv")
+        assert Path(path).filename == "avengers"
+
+    def test_init__extension(self):
+        path = join(self.cwd, "avengers.mkv")
+        assert Path(path).extension == ".mkv"
+
+    def test_init__extension__none(self):
+        path = join(self.cwd, "avengers")
+        assert Path(path).extension == ""
+
+    def tests_full__relative(self):
+        path = "./avengers.mkv"
+        assert Path(path).full == abspath(path)
+
+    def test_full__absolute(self):
+        path = join(self.cwd, "avengers.mkv")
+        assert Path(path).full == path
