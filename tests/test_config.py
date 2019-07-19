45e9b624fa004203c7b0976bbd0c659a3009e479
Jessy Williams <jwilliams@freshbooks.com>
2019-07-19 08:47:00 -0400

    Adds TestConstructor

---
+++ tests/test_config.py
@@ -0,0 +1,54 @@
+from copy import deepcopy
+from unittest.mock import patch
+
+import pytest
+
+from mnamer import DIRECTIVE_KEYS, PREFERENCE_DEFAULTS
+from mnamer.config import Configuration
+from mnamer.exceptions import MnamerConfigException
+from tests import JUNK_TEXT
+
+DEFAULT_CONFIG = {
+    **PREFERENCE_DEFAULTS,
+    **{directive: False for directive in DIRECTIVE_KEYS},
+}
+
+
+class TestConstructor:
+    def test_defaults_only(self):
+        config = Configuration()
+        assert config._dict == DEFAULT_CONFIG
+
+    def test_config_overlay(self):
+        expect = deepcopy(DEFAULT_CONFIG)
+        expect["verbose"] = True  # default is False
+        with patch("mnamer.config.isfile") as mock_isfile:
+            mock_isfile.return_value = True
+            with patch("mnamer.config.access") as mock_access:
+                mock_access.return_value = True
+                with patch("mnamer.config.json_read") as json_read:
+                    json_read.return_value = {"verbose": True}
+                    config = Configuration(config_file=JUNK_TEXT)
+        assert config._dict == expect
+
+    def test_config_overlay__rejects_not_found(self):
+        with pytest.raises(MnamerConfigException):
+            Configuration(config_file=JUNK_TEXT)
+
+    def test_config_overlay__rejects_bad_keys(self):
+        with pytest.raises(MnamerConfigException):
+            Configuration(apples=5)
+
+    def test_override_overlay(self):
+        config = Configuration(verbose=True)
+        assert config["verbose"] is True
+
+    def test_config_and_override_overlay(self):
+        with patch("mnamer.config.isfile") as mock_isfile:
+            mock_isfile.return_value = True
+            with patch("mnamer.config.access") as mock_access:
+                mock_access.return_value = True
+                with patch("mnamer.config.json_read") as json_read:
+                    json_read.return_value = {"verbose": True}
+                    config = Configuration(config_file=JUNK_TEXT, verbose=False)
+        assert config._dict == DEFAULT_CONFIG
