93b2078af361285d3209406a0fc088f3a0fa1891
Jessy Williams <jessy@jessywilliams.com>
2019-08-31 14:23:01 -0400

    Adds Result helper class.

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -2,12 +2,25 @@ import json
 import re
 from os.path import join
 from unittest.mock import patch
-
 import pytest
 
 from mnamer import HELP, PREFERENCE_DEFAULTS
 from mnamer.__version__ import VERSION
 from mnamer.exceptions import MnamerAbortException, MnamerSkipException
+
+
+class Result:
+    passed: int
+    total: int
+
+    def __init__(self, out: str):
+        assert out
+        self._out = out
+        last_line = out.splitlines()[-1:][0]
+        self.passed, self.total = [int(s) for s in last_line if s.isdigit()]
+
+    def has_moved(self, destination: str) -> bool:
+        return re.search(f"moving to .+{destination}", self._out) is not None
 
 
 @pytest.mark.usefixtures("reset_params")
@@ -22,11 +35,13 @@ def test_targets__none(e2e_main):
 @pytest.mark.usefixtures("setup_test_path")
 def test_targets__television__single_file(e2e_main):
     out, err = e2e_main("--batch", "game.of.thrones.01x05-eztv.mp4")
-    assert re.search(
-        r"moving to .+Game of Thrones - S01E05 - The Wolf and The Lion.mp4", out
-    )
-    assert out.endswith("1 out of 1 files processed successfully")
+    result = Result(out)
     assert not err
+    assert result.has_moved(
+        "Game of Thrones - S01E05 - The Wolf and The Lion.mp4"
+    )
+    assert result.passed == 1
+    assert result.total == 1
 
 
 @pytest.mark.usefixtures("reset_params")
@@ -37,20 +52,25 @@ def test_targets__television__multi_file(e2e_main):
         "game.of.thrones.01x05-eztv.mp4",
         join("Downloads", "archer.2009.s10e07.webrip.x264-lucidtv.mkv"),
     )
-    assert re.search(
-        r"moving to .+Game of Thrones - S01E05 - The Wolf and The Lion.mp4", out
-    )
-    assert out.endswith("2 out of 2 files processed successfully")
+    result = Result(out)
     assert not err
+    assert result.has_moved(
+        "Game of Thrones - S01E05 - The Wolf and The Lion.mp4"
+    )
+    assert result.passed == 2
+    assert result.total == 2
 
 
 @pytest.mark.usefixtures("reset_params")
 @pytest.mark.usefixtures("setup_test_path")
 def test_targets__movie__single_file(e2e_main):
     out, err = e2e_main("--batch", "avengers infinity war.wmv")
-    assert re.search(r"moving to .+Avengers Infinity War \(2018\).wmv", out)
-    assert out.endswith("1 out of 1 files processed successfully")
+    result = Result(out)
     assert not err
+
+    assert result.has_moved(r"Avengers Infinity War \(2018\).wmv")
+    assert result.passed == 1
+    assert result.total == 1
 
 
 @pytest.mark.usefixtures("reset_params")
@@ -61,9 +81,11 @@ def test_targets__movie__multi_file(e2e_main):
         "avengers infinity war.wmv",
         join("Downloads", "Return of the Jedi 1080p.mkv"),
     )
-    assert re.search(r"moving to .+Avengers Infinity War \(2018\).wmv", out)
-    assert re.search(r"moving to .+Return of the Jedi \(1983\).mkv", out)
-    assert out.endswith("2 out of 2 files processed successfully")
+    result = Result(out)
+    assert result.has_moved(r"Avengers Infinity War \(2018\).wmv")
+    assert result.has_moved(r"Return of the Jedi \(1983\).mkv")
+    assert result.passed == 2
+    assert result.total == 2
     assert not err
 
 
@@ -75,11 +97,11 @@ def test_targets__mixed(e2e_main):
         "game.of.thrones.01x05-eztv.mp4",
         join("Downloads", "Return of the Jedi 1080p.mkv"),
     )
-    assert re.search(
-        r"moving to .+Game of Thrones - S01E05 - The Wolf and The Lion.mp4", out
-    )
-    assert re.search(r"moving to .+Return of the Jedi \(1983\).mkv", out)
-    assert out.endswith("2 out of 2 files processed successfully")
+    result = Result(out)
+    assert result.has_moved(r"Game of Thrones - S01E05 - The Wolf and The Lion.mp4")
+    assert result.has_moved(r"Return of the Jedi \(1983\).mkv")
+    assert result.passed == 2
+    assert result.total == 2
     assert not err
 
 
@@ -87,19 +109,24 @@ def test_targets__mixed(e2e_main):
 @pytest.mark.usefixtures("setup_test_path")
 def test_targets__mixed__single_directory(e2e_main):
     out, err = e2e_main("--batch", ".")
-    assert re.search(
-        r"moving to .+Game of Thrones - S01E05 - The Wolf and The Lion.mp4", out
+    result = Result(out)
+    assert result.has_moved(
+        r"Game of Thrones - S01E05 - The Wolf and The Lion.mp4"
     )
-    assert re.search(r"moving to .+Ninja Turtles \(1990\).mkv", out)
-    assert re.search(r"moving to .+Avengers Infinity War \(2018\).wmv", out)
-    assert out.endswith("3 out of 3 files processed successfully")
+    assert result.has_moved(r"Ninja Turtles \(1990\).mkv")
+    assert result.has_moved(r"Avengers Infinity War \(2018\).wmv")
+    assert result.passed == 3
+    assert result.total == 3
     assert not err
 
 
 @pytest.mark.usefixtures("reset_params")
 @pytest.mark.usefixtures("setup_test_path")
 def test_targets__mixed__recursive(e2e_main):
-    out, err = e2e_main()
+    out, err = e2e_main("--batch", "--recurse", ".")
+    result = Result(out)
+    assert result.passed == 7
+    assert result.total == 7
     assert not err
 
 
@@ -135,7 +162,9 @@ def test_directives__version(e2e_main, flag):
 def test_skip(mock_prompt, e2e_main):
     mock_prompt.side_effect = MnamerSkipException()
     out, err = e2e_main(".")
-    assert out.endswith("0 out of 3 files processed successfully")
+    result = Result(out)
+    assert result.passed == 0
+    assert result.total == 3
     assert not err
 
 
@@ -145,5 +174,7 @@ def test_skip(mock_prompt, e2e_main):
 def test_abort(mock_prompt, e2e_main):
     mock_prompt.side_effect = MnamerAbortException()
     out, err = e2e_main(".")
-    assert out.endswith("0 out of 3 files processed successfully")
+    result = Result(out)
+    assert result.passed == 0
+    assert result.total == 3
     assert not err
