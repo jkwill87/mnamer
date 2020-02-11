a778352d7410816f8433595a8daab4253348bef9
Jessy Williams <jessy@jessywilliams.com>
2020-02-10 21:29:13 -0500

    Adds test_noreplace

--- tests/e2e/test_moving.py
+++ tests/e2e/test_moving.py
@@ -14,14 +14,22 @@ def test_complex_metadata(e2e_run: Callable):
 def test_batch(e2e_run: Callable):
     result = e2e_run("--batch", ".")
     assert result.code == 0
-    assert "12 out of 12 files processed successfully" in result.out
+    assert "11 out of 12 files processed successfully" in result.out
 
 
-def test_batch__noguess(e2e_run: Callable):
+def test_noguess(e2e_run: Callable):
     result = e2e_run("--noguess", "--batch", ".")
     assert result.code == 0
-    assert result.out.count("skipping (noguess)") == 3
+    assert result.out.count("skipping (--noguess)") == 3
     assert "9 out of 12 files processed successfully" in result.out
+
+
+def test_noreplace(e2e_run: Callable):
+    open("Aladdin (1992).avi", "w").close()
+    result = e2e_run("--noreplace", "--batch", "aladdin.1992.avi")
+    assert result.code == 0
+    assert result.out.count("skipping (--noreplace)") == 1
+    assert "0 out of 1 files processed successfully" in result.out
 
 
 def test_ignore(e2e_run: Callable):
@@ -38,7 +46,7 @@ def test_mask(e2e_run: Callable):
     assert ".mkv" not in result.out
     assert ".avi" not in result.out
     assert ".mp4" in result.out
-    assert "7 out of 7 files processed successfully" in result.out
+    assert "6 out of 7 files processed successfully" in result.out
 
 
 def test_multi_part_episode(e2e_run: Callable):
