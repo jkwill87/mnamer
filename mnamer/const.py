729cf87df610b8c1757db7dee338572fd83a6430
Jessy Williams <jessy@jessywilliams.com>
2022-02-06 23:20:34 -0500

    Makes version detection resiliant to ModuleNotFoundError errors

--- mnamer/const.py
+++ mnamer/const.py
@@ -5,12 +5,37 @@ from pathlib import Path
 from platform import platform, python_version
 from sys import argv, gettrace, version_info
 
-from appdirs import __version__ as appdirs_version  # type: ignore
-from appdirs import user_cache_dir
-from guessit import __version__ as guessit_version
-from requests import __version__ as requests_version
-from requests_cache import __version__ as requests_cache_version
-from teletype import VERSION as teletype_version
+try:
+    from appdirs import __version__ as appdirs_version  # type: ignore
+except ModuleNotFoundError:
+    appdirs_version = "N/A"
+
+try:
+    from appdirs import user_cache_dir
+
+    cache_dir = user_cache_dir()
+except ModuleNotFoundError:
+    cache_dir = "N/A"
+
+try:
+    from guessit import __version__ as guessit_version
+except ModuleNotFoundError:
+    guessit_version = "N/A"
+
+try:
+    from requests import __version__ as requests_version
+except ModuleNotFoundError:
+    requests_version = "N/A"
+
+try:
+    from requests_cache import __version__ as requests_cache_version
+except ModuleNotFoundError:
+    requests_cache_version = "N/A"
+
+try:
+    from teletype import VERSION as teletype_version
+except ModuleNotFoundError:
+    teletype_version = "N/A"
 
 from mnamer.__version__ import VERSION
 
@@ -28,7 +53,7 @@ __all__ = [
 
 
 CACHE_PATH = Path(
-    user_cache_dir(), f"mnamer-py{version_info.major}.{version_info.minor}"
+    cache_dir, f"mnamer-py{version_info.major}.{version_info.minor}"
 ).absolute()
 
 CURRENT_YEAR = datetime.now().year
