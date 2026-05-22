1bf655386e90f855f3ee6891908173a8205279a7
Jessy Williams <jessy@jessywilliams.com>
2026-05-22 10:59:16 -0400

    Add e2e tests for --lower and --scene on configured directories

--- tests/e2e/test_moving.py
+++ tests/e2e/test_moving.py
@@ -145,6 +145,40 @@ def test_multiple_nested_directories(e2e_run, setup_test_files):
     assert expected in result.out
 
 
+@pytest.mark.usefixtures("setup_test_dir")
+def test_lower_directory(e2e_run, setup_test_files):
+    setup_test_files("Ninja Turtles (1990).mkv")
+    result = e2e_run(
+        "--batch",
+        "--lower",
+        "--movie_directory=Movies/{name[0]}",
+        "Ninja Turtles (1990).mkv",
+    )
+    expected = str(Path("/movies/t/teenage mutant ninja turtles (1990).mkv"))
+    assert result.code == 0
+    assert expected in result.out
+
+
+@pytest.mark.usefixtures("setup_test_dir")
+def test_scene_directory(e2e_run, setup_test_files):
+    setup_test_files("Ninja Turtles (1990).mkv")
+    result = e2e_run(
+        "--batch",
+        "--scene",
+        "--movie_directory=Movie Library/{name}",
+        "Ninja Turtles (1990).mkv",
+    )
+    expected = str(
+        Path(
+            "/movie.library"
+            "/teenage.mutant.ninja.turtles"
+            "/teenage.mutant.ninja.turtles.1990.mkv"
+        )
+    )
+    assert result.code == 0
+    assert expected in result.out
+
+
 @pytest.mark.omdb
 @pytest.mark.usefixtures("setup_test_dir")
 def test_format_id(e2e_run, setup_test_files):
